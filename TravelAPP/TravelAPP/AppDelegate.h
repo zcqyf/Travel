248462865f4e515d3cb7d058eb420f1b95739263
qing <qing@qingdeMacBook-Pro.local>
2017-05-01 08:04:37 +0800

    初始提交

---
+++ TravelAPP/TravelAPP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TravelAPP
+//
+//  Created by qing on 2017/5/1.
+//  Copyright © 2017年 qing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TravelAPP/TravelAPP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TravelAPP
+//
+//  Created by qing on 2017/5/1.
+//  Copyright © 2017年 qing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
