32fc474443ec97b018d5774219b369fec60efabb
qing <qing@qingdeMacBook-Pro.local>
2017-05-18 00:06:52 +0800

    我的界面补充

--- TravelAPP/TravelAPP/Main/Mine(我的)/Controller/AboutUsViewController.m
+++ TravelAPP/TravelAPP/Main/Mine(我的)/Controller/AboutUsViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "AboutUsViewController.h"
+#import "AboutUsTableViewCell.h"
+#import <Masonry.h>
 
-@interface AboutUsViewController ()
+@interface AboutUsViewController ()<UITableViewDelegate, UITableViewDataSource>
+
+@property (nonatomic,strong)UITableView *tableView;
+
+@property (nonatomic,strong)NSArray *data;
 
 @end
 
@@ -16,7 +22,77 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self setupUI];
+}
+
+- (void)setupUI {
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    [self.view addSubview:self.tableView];
+    [_tableView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(64);
+        make.left.right.bottom.equalTo(self.view);
+    }];
+}
+
+- (UITableView *)tableView {
+    if (!_tableView) {
+        _tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+        _tableView.backgroundColor = [UIColor colorWithRed:0.96 green:0.96 blue:0.96 alpha:1.0];
+        _tableView.delegate = self;
+        _tableView.dataSource = self;
+        _tableView.tableFooterView = [UIView new];
+        [_tableView registerNib:[UINib nibWithNibName:@"AboutUsTableViewCell" bundle:[NSBundle mainBundle]] forCellReuseIdentifier:@"AboutUsTableViewCell"];
+        [_tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"UITableViewCell"];
+    }
+    return _tableView;
+}
+
+- (NSArray *)data {
+    if (!_data) {
+        _data = @[@"企业名称", @"地址", @"电话", @"邮箱", @"微博", @"微信"];
+    }
+    return _data;
+}
+
+
+#pragma tableView dataSource
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 7;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.row == 0) {
+        AboutUsTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"AboutUsTableViewCell" forIndexPath:indexPath];
+        return cell;
+    } else {
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell" forIndexPath:indexPath];
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+        cell.textLabel.font = [UIFont systemFontOfSize:14];
+        cell.textLabel.text = self.data[indexPath.row - 1];
+        return cell;
+    }
+}
+
+
+#pragma tableView delegate
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.row == 0) {
+        return 160;
+    } else {
+        return 44;
+    }
+}
+
+//- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section {
+//    return 5;
+//}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
 }
 
 - (void)didReceiveMemoryWarning {
