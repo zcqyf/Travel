b58aaf3954126eca567b790c38fe9dc4def88dbf
Gene_Ye <yezijun1111@icloud.com>
2017-05-19 14:17:22 +0800

    添加myInfo单例，用于存储个人信息

---
+++ TravelAPP/TravelAPP/Expand/Category/MyInfo.h
@@ -0,0 +1,16 @@
+//
+//  MyInfo.h
+//  TravelAPP
+//
+//  Created by Gene_Ye on 2017/5/19.
+//  Copyright © 2017年 qing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyInfo : NSObject
+
+//1: 付费会员  2: 普通会员
+@property (nonatomic, assign)NSInteger *MemberType;
+
+@end
---
+++ TravelAPP/TravelAPP/Expand/Category/MyInfo.m
@@ -0,0 +1,15 @@
+//
+//  MyInfo.m
+//  TravelAPP
+//
+//  Created by Gene_Ye on 2017/5/19.
+//  Copyright © 2017年 qing. All rights reserved.
+//
+
+#import "MyInfo.h"
+
+@implementation MyInfo
+
+
+
+@end
