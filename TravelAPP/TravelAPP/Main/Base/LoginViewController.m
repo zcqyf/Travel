5faba9b140d3ae9dfa537a5fd28d8f929c32017b
Gene_Ye <yezijun1111@icloud.com>
2017-05-21 18:13:14 +0800

    添加MyInfo，用于存储用户数据

--- TravelAPP/TravelAPP/Main/Base/LoginViewController.m
+++ TravelAPP/TravelAPP/Main/Base/LoginViewController.m
@@ -8,29 +8,87 @@
 
 #import "LoginViewController.h"
 #import "RigsterViewController.h"
+#import "MyInfo.h"
 //#import "UserLogin.h"
 
 @interface LoginViewController ()
-    @property (weak, nonatomic) IBOutlet UITextField *accountTextField;
-    @property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+//账号
+@property (weak, nonatomic) IBOutlet UITextField *accountTextField;
+//密码
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
 
-    @property (weak, nonatomic) IBOutlet UISegmentedControl *topSegementControl;
+@property (nonatomic, strong)MyInfo *myInfo;
+
+
+/*
+ 选择付费／普通会员
+ */
+
+@property (weak, nonatomic) IBOutlet UIView *segementBgView;
+//付费会员:101
+@property (weak, nonatomic) IBOutlet UIButton *PayMemberBtn;
+//普通会员:102
+@property (weak, nonatomic) IBOutlet UIButton *ordinaryMemberBtn;
+
+
+
+
 @end
 
 @implementation LoginViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    //默认为付费会员
+    
     [self.navigationController.navigationBar setBackgroundImage:[UIImage new] forBarMetrics:UIBarMetricsDefault];
     [self.navigationController.navigationBar setShadowImage:[UIImage new]];
+    
+    [self setUpView];
+    
+    
+    
+}
+- (void)setUpView {
+    self.segementBgView.layer.borderWidth = 1;
+    self.segementBgView.layer.borderColor = [UIColor whiteColor].CGColor;
+    self.segementBgView.layer.cornerRadius = 15.0;
+    self.segementBgView.layer.masksToBounds = YES;
+    
+    _PayMemberBtn.tag = 101;
+    _ordinaryMemberBtn.tag = 102;
+    
+      [MyInfo shareInstance].MemberType = _PayMemberBtn.tag-100;
+
 }
 
 /*
  选择：付费会员／普通会员
  */
-- (IBAction)selectMemberTypeAction:(id)sender {
+- (IBAction)selectMemberType:(UIButton *)sender {
+    if (sender.tag == _PayMemberBtn.tag) {
+        _PayMemberBtn.backgroundColor = [UIColor whiteColor];
+        [_PayMemberBtn setTitleColor:LoginSegementColor forState:UIControlStateNormal];
+        
+        _ordinaryMemberBtn.backgroundColor = [UIColor clearColor];
+        [_ordinaryMemberBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    }else if (sender.tag == _ordinaryMemberBtn.tag) {
+        _PayMemberBtn.backgroundColor = [UIColor clearColor];
+        [_PayMemberBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        
+        _ordinaryMemberBtn.backgroundColor = [UIColor whiteColor];
+        [_ordinaryMemberBtn setTitleColor:LoginSegementColor forState:UIControlStateNormal];
+    }
+    [MyInfo shareInstance].MemberType = sender.tag-100;
     
 }
+
+
+
+
+
+
 
 /*
  注册
